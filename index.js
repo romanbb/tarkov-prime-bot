a5e4696288b7a6b8d04b3da59a718676a4ffbeac
Roman Birg <romanbirg@gmail.com>
2022-07-19 23:55:48 -0700

    feat: basic recording for user with !start

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
+const config = require('./config.json');
+const fs = require('fs');
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
-	}
+    if (message.content === '!ping') {
+        message.channel.send('Pong.');
+    } else if (message.content == '!start') {
+        listenToUser(message.member.voice.channel, message.member);
+    }
 });
 
 client.login(config.token);
+
+/**
+ * 
+ * @param {Discord.VoiceChannel} voiceChannel 
+ * @param {Discord.GuildMember} user
+ */
+async function listenToUser(voiceChannel, user) {
+    const connection = await voiceChannel.join();
+
+    const audio = connection.receiver.createStream(user, {
+        mode: "pcm"
+    })
+    audio.addListener("close", () => {
+        console.log("Event :: close")
+
+        // setup a new stream
+        listenToUser(voiceChannel, user);
+    })
+
+    audio.pipe(fs.createWriteStream(`${user.id}-audio-${new Date().getTime()}.pcm`))
+}
 
