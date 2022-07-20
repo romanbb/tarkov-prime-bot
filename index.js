1354e20ec7a9d0efade626ed23fb678c32988074
Roman Birg <romanbirg@gmail.com>
2022-07-19 22:11:13 -0700

    feat: add pong!

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		message.channel.send('Pong.');
+	}
+});
+
+client.login(config.token);
+
