b54579c9f74501fb2826bb49524e5e2047e0d3c5
Roman Birg <romanbirg@gmail.com>
2024-01-06 01:16:02 -0800

    update configs

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,4 @@ module.exports = {
     singleQuote: false, // Force using single quotes
     trailingComma: 'all', // Force the use of a comma at the end of objects
     arrowParens: 'avoid', // Force not using parentheses with single parameter functions.
-    tabWidth: 4, // Force using 4 spaces for indentation
   }
